87ecb29729b68dbb0d340cd7f2faf2590dd23aee
GScottJ@Macbook <g.scott.j@usask.ca>
2014-11-11 19:19:43 -0600

    no-gui-server: added a script to run a server without a gui; no gradle.

---
+++ serverRunner.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+# Runs org.ggp.base.apps.utilities.GameServerRunner, a command-line only server. 
+# Just a small layer of script.  Less of something that needs to be a script, 
+# more just a way to write down the fact that you need to change the classpath
+# and it has to be pretty specific.  
+
+lib_st="lib/"
+cp_var="bin/"
+cp_var="${cp_var}:${lib_st}Batik/*"
+cp_var="${cp_var}:${lib_st}Clojure/*"
+cp_var="${cp_var}:${lib_st}FlyingSaucer/*"
+cp_var="${cp_var}:${lib_st}Guava/*"
+cp_var="${cp_var}:${lib_st}Htmlparser/*"
+cp_var="${cp_var}:${lib_st}JFreeChart/*"
+cp_var="${cp_var}:${lib_st}JGoodiesForms/*"
+cp_var="${cp_var}:${lib_st}JUnit/*"
+cp_var="${cp_var}:${lib_st}Jython/*"
+cp_var="${cp_var}:${lib_st}javassist/*"
+cp_var="${cp_var}:${lib_st}reflection/*"
+
+# Good defaults for the arguments to this script:
+# testing ticTacToe 60 30 localhost 9147 First localhost 9148 Second
+# Note that you'll need to have two players running, with those ports. 
+# The easiest way to get two players running with those ports is to run 
+# ./gradlew player
+# and then create two new players.  914{7,8} are the default ports of the first
+# two created players. 
+
+java -cp $cp_var org/ggp/base/apps/utilities/GameServerRunner $@
+

46de300a6076b35bd6d7d7018f5beded4b72a70a
GScottJ@Macbook <g.scott.j@usask.ca>
2014-11-24 16:47:37 -0600

    worker.ggpPlayerProcess: added params to constructor for ggp-base PlayerRunner class's args.

--- src/main/python/worker/ggpPlayerProcess.py
+++ src/main/python/worker/ggpPlayerProcess.py
@@ -17,15 +17,31 @@ class GGPPlayerProcess(util.java_runner.JavaProcess):
 	# This should be externalized with a config file
 	default_ggpPlayerClass_loc = "org/ggp/base/apps/player/PlayerRunner"
 
-	"""GGPPlayerProcess.__init__(self, args)
+	"""GGPPlayerProcess.__init__(self, port, playerClass)
 		Initialize the GGPPlayerProcess.  
 		The class to run is not a parameter, but instead is externalized. 
-		The arguments are currently temporary; should be constructed based on 
-		fields, such as player hosts/ports.  
+		port: which port to listen on for play moves
+		playerClass: which player to play as
 	"""
-	def __init__(self, args=[]):
+	def __init__(self, port, playerClass):
+		# Call superconstructor
 		self._ggpPlayerClass_loc = self._construct_ggpPlayerClass_loc()
-		super(GGPPlayerProcess, self).__init__(self._ggpPlayerClass_loc, args)
+		super(GGPPlayerProcess, self).__init__(self._ggpPlayerClass_loc)
+		
+		# Set fields:
+		self._port = port
+		self._playerClass = playerClass
+		
+		# Initialize args list:
+		# DEPENDENCY: static processing of arguments in 
+		# 	org/ggp/base/apps/player/PlayerRunner
+		self.args = []
+		# args[0]: port: which port to communicate with game server over
+		self.args.append(str(self._port))
+		# args[1]: name: the class name of the player to play as
+		self.args.append(self._playerClass)
+		
+	
 	
 	"""_construct_ggpPlayerClass_loc():
 		 --> string

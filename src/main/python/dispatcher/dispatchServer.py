5397d06e60477debd7b844d875b48acad10576b5
GScottJ@Macbook <g.scott.j@usask.ca>
2014-11-28 13:25:23 -0600

    D/W_comm: added a StreamRequestHandler that handles connections from worker saying its ready to play.

--- src/main/python/dispatcher/dispatchServer.py
+++ src/main/python/dispatcher/dispatchServer.py
@@ -2,6 +2,8 @@
 
 
 import Queue
+import SocketServer
+import json
 
 
 
@@ -37,5 +39,34 @@ class PlayerHostQueue(object):
 		return _queue.get(True)
 	
 	
+
+
+
+
+# Listen for incoming connections from ready Workers
+
+
+class ReadyWorkerHandler(SocketServer.StreamRequestHandler):
+	"""
+	Based partially on this:
+		http://thomasfischer.biz/python-simple-json-tcp-server-and-client/
+	Partially on Python docs:
+		https://docs.python.org/2/library/socketserver.html
+	"""
+	
+	
+	def handle(self):
+		# Read in from the stream:
+		read_data = self.rfile.readline().strip()
+		# Return an okay on the socket.  
+		self.request.sendall(json.dumps({'return':'ok'}))
+		
+		# Parse the data as json:
+		data = json.load(read_data)
+		# Get out the fields we want:
+		hostname, port = (data.hostname, data.port)
+		# Add the ready worker to the queue.  
+		PlayerHostQueue.put_host(hostname, port)
+		
 
 

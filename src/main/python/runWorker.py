4fc350280c4e036da10781daf90afedd9b6cd92c
GScottJ@Macbook <g.scott.j@usask.ca>
2014-12-08 00:28:51 -0600

    py-logging: added logging to run{Dispatch,Worker}.py scripts...
    - Uses config file logging.d.conf, logging.w.conf
    - Takes -d/--debug, and sets logging of console level to DEBUG if seen
    - Takes -v/--verbose and sets logging of console level to INFO if seen
    - Default console log level is WARN

--- src/main/python/runWorker.py
+++ src/main/python/runWorker.py
@@ -1,7 +1,15 @@
 
+# Local imports
 import worker.workerServer
+import util.logging_help
 
+# Library imports
 import optparse
+
+# Set up logging
+import logging
+import logging.config
+logging.config.fileConfig("logging.w.conf",disable_existing_loggers=False)
 
 
 parser = optparse.OptionParser()
@@ -15,11 +23,40 @@ parser.add_option("-w", "--wPort",
 		help="What port to run the worker with.",
 		default=21000
 		)
+parser.add_option("-d", "--debug",
+		dest="debug",
+		help="Turn logging level to DEBUG.",
+		action="store_true"
+		)
+parser.add_option("-v", "--verbose",
+		dest="verbose",
+		help="Turn logging level to VERBOSE.",
+		action="store_true"
+		)
 (options, args) = parser.parse_args()
+
+# Change the file location for this worker, now that we know the port.
+util.logging_help.set_fileHandler_file(
+		logging.getLogger(),
+		"testing/worker." + str(options.wPort) + ".log")
+
+if options.debug:
+	util.logging_help.set_streamHandler_levels(
+			logging.getLogger(),
+			logging.DEBUG)
+	logging.getLogger("worker").debug("Set log level to debug.")
+elif options.verbose:
+	util.logging_help.set_streamHandler_levels(
+			logging.getLogger(),
+			logging.INFO)
+else:
+	util.logging_help.set_streamHandler_levels(
+			logging.getLogger(),
+			logging.WARN)
+
 
 pPort = int(options.pPort)
 wPort = int(options.wPort)
 ws = worker.workerServer.WorkerServer(pPort, wPort)
 ws.run()
-
 

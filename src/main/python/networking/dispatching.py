363e82059e76daf3f7d916d5f748fe53cf100a9c
GScottJ@Macbook <g.scott.j@usask.ca>
2015-04-06 21:11:25 -0600

    refactor: dispatch ready worker listening now moved to network.dispatching.DispatchReadyWorkerServer and [...].ReadyWorkerHandler.

--- src/main/python/networking/dispatching.py
+++ src/main/python/networking/dispatching.py
@@ -4,7 +4,7 @@
 # Library imports
 import socket
 import json
-
+import SocketServer
 
 
 # Local imports
@@ -13,7 +13,7 @@ import server
 
 
 
-class DispatchAnnounceMatchServer(Server):
+class DispatchAnnounceMatchServer(server.Server):
     
     def __init__(self, logger, configuration, playerHostWorkerTuple):
         self._logger = logger
@@ -36,6 +36,53 @@ class DispatchAnnounceMatchServer(Server):
         finally:
             s.close()
         
+
+
+
+# Listen for incoming connections from ready Workers
+
+
+class DispatchReadyWorkerServer(SocketServer.ThreadingTCPServer):
+    
+    def __init__(self, address_tuple, playerHost_queue):
+        SocketServer.ThreadingTCPServer(address_tuple, ReadyWorkerHandler)
+        
+        self._queue = playerHost_queue
+
+
+
+
+class ReadyWorkerHandler(SocketServer.StreamRequestHandler):
+    """
+    Based partially on this:
+        http://thomasfischer.biz/python-simple-json-tcp-server-and-client/
+    Partially on Python docs:
+        https://docs.python.org/2/library/socketserver.html
+    """
+    
+    def handle(self):
+        """ReadyWorkerHandler.handle
+            Reads from the rfile; load it from json; send an okay back; 
+            get out the hostname and port that was sent to us from the worker
+            that reported ready; and then add that player to our queue of 
+            available workers.  
+        """
+        LOG.debug("Handling ready worker connection.")
+        # Read in from the stream:
+        # Parse the data as json:
+        data = json.load(self.rfile)
+        # Return an okay on the socket.  
+        self.request.sendall(json.dumps({'return':'ok'}))
+        
+        # Get out the fields we want:
+        hostname, pPort = (data["hostname"], data["pPort"])
+        wPort = data["wPort"]
+        LOG.info("Handled worker was %s, %s, %s", hostname, pPort, wPort)
+        # Add the ready worker to the queue.  
+        self.server._queue.put_host(hostname, pPort, wPort)
+        
+
+
 
 
 

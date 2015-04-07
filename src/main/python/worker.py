0346a1b6187025f48e311ec88ea2bc852fa1f1d6
GScottJ@Macbook <g.scott.j@usask.ca>
2015-04-07 11:08:24 -0600

    interaction: worker now is a state machine, can go back and forth between waiting and announcing ready; also, waits for both, instead of dying -- can be started up without dispatcher on.

--- src/main/python/worker.py
+++ src/main/python/worker.py
@@ -47,44 +47,101 @@ class WorkerServer(object):
         LOG.debug("WorkerServer constructed, %s, %i, %i", 
                 self._ourHostname, self._ourWorkerPort, self._ourPlayerPort)
         
-        self.running = None
+        self._announce_max = 5
+        self._announce_sleep = 5
+        self._wait_max = 1
+        self._wait_sleep = 5
+        
+    
+    def is_running(self):
+        return self._state > 0
+    
+    def start(self):
+        self._state = 1
+        
+        self._announce_count = 0
+        self._wait_count = 0
+        
+    def _set_announcing(self):
+        self._state = 1
+        self._announce_count = 0
+        
+    def _set_waiting(self):
+        self._state = 2
+        self._wait_count = 0
+    
+    def _set_playing(self):
+        self._state = 3
+    
+    def _set_finished(self):
+        self._state = 0
+    
+    def is_announcing(self):
+        return self._state == 1
+    
+    def is_waiting(self):
+        return self._state == 2
+    
+    def is_playing(self):
+        return self._state == 3
+    
+    def _announce_worked(self):
+        self._set_waiting()
+    
+    def _announce_failed(self):
+        if self._announce_count == self._announce_max:
+            LOG.info("WorkerServer announced ready " + \
+                str(self._announce_max) + " times, giving up.")
+            self._set_finished()
+        else:
+            LOG.debug("WorkerServer could not announce ready, waiting for " + \
+                str(self._announce_sleep) + "s.")
+            self._announce_count += 1
+            time.sleep(self._announce_sleep)
+    
+    def _wait_worked(self):
+        self._set_playing()
+    
+    def _wait_failed(self):
+        if self._wait_count == self._wait_max:
+            LOG.info("WorkerServer waited for match details " + \
+                str(self._wait_max) + " times, announcing ready again.")
+            self._set_announcing()
+        else:
+            LOG.debug("WorkerServer could not get match details, waiting " + \
+                "for " + str(self._wait_sleep) + "s.")
+            self._wait_count += 1
+            time.sleep(self._wait_sleep)
+    
+    def _play_worked(self):
+        self._set_announcing()
+    
+    def _play_failed(self):
+        self._set_announcing()
     
     def do_announce_ready(self):
-        if self.running:
-            # Tell the dispatcher we're ready to play a game.  
-            wars = NET.WorkerAnnounceReadyServer(
-                self._dispatcher_hp,
-                self._ourHostname, self._ourPlayerPort, self._ourWorkerPort
-                )
-            wars.run()
-            self.running = wars.finished() and wars.successful()
+        # Tell the dispatcher we're ready to play a game.  
+        wars = NET.WorkerAnnounceReadyServer(
+            self._dispatcher_hp,
+            self._ourHostname, self._ourPlayerPort, self._ourWorkerPort
+            )
+        wars.run()
+        return wars.finished() and wars.successful()
     
     def do_wait(self):
         data = None
-        max_times = 1
-        times = 1
-        while self.running and data == None:
-            # The wait half:
-            wgmps = NET.WorkerGetMatchParamsServer(
+        wgmps = NET.WorkerGetMatchParamsServer(
                 self._ourHostname, self._ourWorkerPort
                 )
-            try:
-                wgmps.run()
-                LOG.debug("WorkerServer got match to play.")
-            except NET.WorkerGetMatchParamsServerAllowableError as e:
-                LOG.debug(e)
-                time.sleep(5)
-            except Exception as e:
-                LOG.warn(e)
-                self.running = False
-            if wgmps.finished() and wgmps.successful():
-                data = wgmps.response()
-            if times == max_times:
-                LOG.info("WorkerServer tried to get match " + \
-                    str(max_times) + " times; shutting down.")
-                self.running = False
-            else:
-                times = times + 1
+        try:
+            wgmps.run()
+            LOG.debug("WorkerServer got match to play.")
+        except NET.WorkerGetMatchParamsServerAllowableError as e:
+            LOG.debug(e)
+        except Exception as e:
+            LOG.warn(e)
+        if wgmps.finished() and wgmps.successful():
+            data = wgmps.response()
         return data
     
     def do_play(self, data):
@@ -103,14 +160,29 @@ class WorkerServer(object):
         """WorkerServer.run(): just loops: announcing ready, wait to play.
             Loops as long as self.running is True, in case of error. 
         """
-        self.running = True
-        while self.running:
+        self.start()
+        while self.is_running():
             # Tell the dispatcher we're ready to play a game.
-            self.do_announce_ready()
+            if self.is_announcing():
+                success = self.do_announce_ready()
+                if success:
+                    self._announce_worked()
+                else:
+                    self._announce_failed()
             # Then wait to hear about what playerType to play as.
-            data = self.do_wait()
+            elif self.is_waiting():
+                data = self.do_wait()
+                if data == None:
+                    self._wait_failed()
+                else:
+                    self._wait_worked()
             # Then play that game. 
-            self.do_play(data)
+            elif self.is_playing():
+                self.do_play(data)
+                self._play_worked()
+            # Otherwise, error:
+            else:
+                raise WorkerServerInvalidStateError()
     
 class WorkerServerConfig(util.config_help.Config):
     
@@ -125,5 +197,10 @@ class WorkerServerConfig(util.config_help.Config):
         }
 
 
+
+class WorkerServerInvalidStateError(RuntimeError):
+    
+    def __init__(self):
+        RuntimeError.__init__(self, "WorkerServer wound up in invalid state.")
 
 

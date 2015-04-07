72270672296ff335625a4c64626c4ddf48f46268
GScottJ@Macbook <g.scott.j@usask.ca>
2015-04-06 21:11:25 -0600

    refactor: dispatch ready worker listening now moved to network.dispatching.DispatchReadyWorkerServer and [...].ReadyWorkerHandler.

--- src/main/python/dispatcher.py
+++ src/main/python/dispatcher.py
@@ -120,40 +120,6 @@ class PlayerHostQueue(object):
 
 
 
-# Listen for incoming connections from ready Workers
-
-
-class ReadyWorkerHandler(SocketServer.StreamRequestHandler):
-    """
-    Based partially on this:
-        http://thomasfischer.biz/python-simple-json-tcp-server-and-client/
-    Partially on Python docs:
-        https://docs.python.org/2/library/socketserver.html
-    """
-    
-    def handle(self):
-        """ReadyWorkerHandler.handle
-            Reads from the rfile; load it from json; send an okay back; 
-            get out the hostname and port that was sent to us from the worker
-            that reported ready; and then add that player to our queue of 
-            available workers.  
-        """
-        LOG.debug("Handling ready worker connection.")
-        # Read in from the stream:
-        # Parse the data as json:
-        data = json.load(self.rfile)
-        # Return an okay on the socket.  
-        self.request.sendall(json.dumps({'return':'ok'}))
-        
-        # Get out the fields we want:
-        hostname, pPort = (data["hostname"], data["pPort"])
-        wPort = data["wPort"]
-        LOG.info("Handled worker was %s, %s, %s", hostname, pPort, wPort)
-        # Add the ready worker to the queue.  
-        PlayerHostQueue.put_host(hostname, pPort, wPort)
-        
-
-
 
 
 
@@ -350,8 +316,8 @@ class DispatchServer(object):
         self._ourDispatchPort = DispatchServer.config.ourDispatchPort
         
         h_p = (self._ourHostname, self._ourDispatchPort)
-        self._readyWorkerServer = SocketServer.ThreadingTCPServer(
-            h_p, ReadyWorkerHandler)
+        self._readyWorkerServer = NET.DispatchReadyWorkerServer(
+            h_p, PlayerHostQueue)
         
         self._tourneyName = DispatchServer.config.tourneyName
         LOG.debug("Dispatch Server constructed.")

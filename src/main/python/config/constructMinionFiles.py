69a325bf25ccd4d514f881caa223b61f791f17e0
GScottJ@Macbook <g.scott.j@usask.ca>
2015-04-24 13:37:36 -0600

    constructMinionFiles.py: can now make sequential config files for current deployment.

--- src/main/python/config/constructMinionFiles.py
+++ src/main/python/config/constructMinionFiles.py
@@ -5,184 +5,65 @@ import json
 import sys
 
 
-GAMES = \
-    [
-        [], # 0 Players
-        [   # 1 Players
-        ],
-        [   # 2 Players
-            "2pffa",
-            #"2pffa_zerosum",
-            "2pttc",
-            #"biddingTicTacToe",
-            #"biddingTicTacToe_10coins",
-            "blocker",
-            "breakthrough",
-            #"breakthroughSmall",
-            #"breakthroughWalls",
-            #"cephalopodMicro",
-            "checkers",
-            #"checkersSmall",
-            #"checkersTiny",
-            "chineseCheckers2",
-            "chinook",
-            #"cittaceot",
-            #"connect5",
-            "connectFour",
-            #"connectFourLarge",
-            #"connectFourLarger",
-            #"connectFourSimultaneous",
-            #"connectFourSuicide",
-            "dotsAndBoxes",
-            #"dotsAndBoxesSuicide",
-            #"englishDraughts",
-            #"escortLatch",
-            #"gt_attrition",
-            #"gt_centipede",
-            #"gt_chicken",
-            "gt_dollar",
-            "gt_prisoner",
-            #"gt_staghunt",
-            #"gt_ultimatum",
-            #"knightThrough",
-            #"nineBoardTicTacToe",
-            "pentago",
-            #"pentagoSuicide",
-            "quarto",
-            #"quartoSuicide",
-            #"qyshinshu",
-            "reversi",
-            #"sheepAndWolf",
-            "speedChess",
-            "ticTacToe",
-            #"ticTacToeLarge",
-            #"ticTacToeLargeSuicide",
-            #"ttcc4_2player"
-        ]
-    ]
 
 
-PLAYERS = [
-    #"SampleClojureGamerStub", 
-    #"SampleSearchLightGamer", 
-    "RandomGamer", 
-    "SampleNoopGamer", 
-    #"SamplePythonGamerStub", 
-    "SampleLegalGamer", 
-    "SampleMonteCarloGamer", 
-    ]
-
-
-
-
-def run(file_name, options, againstSelf, R):
+def run(options):
     
-    n = options.numPlayers
-    
-    matches = []
-    
-    
-    for i in range(len(PLAYERS)):
-        for j in range(len(PLAYERS)):
-            for k in range(len(GAMES[n])):
-                if againstSelf or (not againstSelf and i != j): 
-                    match = construct_match(options)
-                    match["playerTypes"] = [PLAYERS[i], PLAYERS[j]]
-                    match["gameKey"] = GAMES[n][k]
-                    for r in range(R):
-                        matches.append(match)
-    
-    if options.verbose:
-        output = "Num players: " + str(len(PLAYERS)) + "\n"
-        output += "Num games: " + str(len(GAMES[n])) + "\n"
-        output += "Reps: " + str(R) + "\n"
-        output += "Total matches generated: " + str(len(matches))
-        print >> sys.stderr, output
+    num_digits = len(str(options.n))
+    formatter = "%0" + str(num_digits) + "d"
+    for i in range(options.s, options.s + options.n):
+        d = {}
+        d["JavaProcess"] = {}
+        d["JavaProcess"]["ggpBaseInstall_loc"] = "/project/selab/ggp-hardware/"
+        d["WorkerServer"] = {}
+        d["WorkerServer"]["pPort"] = options.p + i 
+        d["WorkerServer"]["wPort"] = options.w + i
+        d["WorkerServer"]["ourHostname"] = "minion" + str(i) 
+        d["WorkerServer"]["dispatchServerAddress"] = "skorpio"  
+        d["WorkerServer"]["dispatchServerPort"] = 35000
         
-    
-    if file_name == None:
-        s = json.dumps(matches, 
+        curr_digit = formatter % i 
+        f_name = options.file_name_start + curr_digit + ".conf"
+        f = open(f_name, "w")
+        json.dump(d, f, 
             sort_keys=True, indent=4, separators=(',', ': '))
-        print s
-    else:
-        f = open(file_name, "w")
-        json.dump(matches, f, 
-            sort_keys=True, indent=4, separators=(',', ': '))
-    
-    
-    
-    
-
-
-def construct_match(options):
-    """construct_match: 
-        Very much a helper, just extract options to dictionary
-    """
-    result = {}
-    result["tourneyName"] = options.tourneyName
-    result["startClock"] = options.startClock
-    result["playClock"] = options.playClock
-    result["numPlayers"] = options.numPlayers
-    return result
-
-
-
-
 
 if __name__ == "__main__":
     parser = optparse.OptionParser()
     parser.add_option("-f", "--file",
-        dest="data_file",
-        help="The output match file to write to; none, and writes to stdout",
+        dest="file_name_start",
+        help="The beginning of the name of the output config files",
         metavar="FILE",
         action="store",
         type="string")
-    parser.add_option("-r", "--numRepetitions",
-        dest="R",
-        help="How many repetitions of each match to include",
+    parser.add_option("-w", "--startingWorkerPort",
+        dest="w",
+        help="The first port to run workers from",
+        action="store",
+        type="int",
+        default=30000)
+    parser.add_option("-p", "--startingPlayerPort",
+        dest="p",
+        help="The first port to run players from",
+        action="store",
+        type="int",
+        default=32000)
+    parser.add_option("-s", "--startNumber",
+        dest="s",
+        help="The first sequential number to make a config file for",
         action="store",
         type="int",
         default=1)
-    parser.add_option("-t", "--tourneyName",
-        dest="tourneyName",
-        help="Name of the tournament to run matches under",
-        action="store",
-        type="str",
-        default=None)
-    parser.add_option("-s", "--startClock",
-        dest="startClock",
-        help="Amount of time before the match starts",
-        action="store",
-        type="int",
-        default=60)
-    parser.add_option("-p", "--playClock",
-        dest="playClock",
-        help="Amount of time for each move in the game",
+    parser.add_option("-n", "--number",
+        dest="n",
+        help="The number of sequential config files to make",
         action="store",
         type="int",
         default=30)
-    parser.add_option("-n", "--numPlayers",
-        dest="numPlayers",
-        help="Amount of time before the match starts",
-        action="store",
-        type="int",
-        default=2)
-    parser.add_option("--self",
-        dest="againstSelf",
-        help="Should players face off against themselves?",
-        action="store_true")
-    parser.add_option("-v", "--verbose", 
-        dest="verbose",
-        help="Prints the clusterer when it's done.",
-        action="store_true")
     (options, args) = parser.parse_args()
     
-    if options.tourneyName == None:
-        print "Please enter a tourney name with -t."
-        sys.exit(1)
-    
     try:
-        run(options.data_file, options, options.againstSelf, options.R)
+        run(options)
     except IOError as e:
         pass
 
